46c263f19b2e1086ecd568a2946a1537d767bb2a
Dikshant1408 <91022943+Dikshant1408@users.noreply.github.com>
2023-03-10 13:43:21 +0530

    Mini Project

---
+++ EulerProject/app/src/main/java/com/example/eulerproject/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.eulerproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ EulerProject/app/src/main/java/com/example/eulerproject/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.eulerproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.example.eulerproject.RegisterActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
